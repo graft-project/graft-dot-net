76169d34647a0d32e8944273a168004d3f837aec
Denys Zaliznyak <dzaliznyak@gmail.com>
2019-03-05 17:00:12 +0200

    BlockNumber added to the ExchangeParameters

--- Infrastructure/Broker/BrokerExchangeParams.cs
+++ Infrastructure/Broker/BrokerExchangeParams.cs
@@ -3,7 +3,8 @@
     // only one amount must be set, all other must be 0
     public class BrokerExchangeParams
     {
-        public string ExchangeId { get; set; }
+        public string PaymentId { get; set; }
+        public int BlockNumber { get; set; }
 
         // fiat equivalent. For example - I want to sell BTC in equivalent to 1$
         // or buy GRFT in equivalent to 1$
@@ -12,10 +13,8 @@
         public decimal SellFiatAmount { get; set; } // how many BTC in fiat equivalent I want to sell
         public decimal BuyFiatAmount { get; set; } // how many GRFT in fiat equivalent I want to buy
 
-
         public string SellCurrency { get; set; } // currency (BTC), I want to sell
         public decimal SellAmount { get; set; } // amount BTC, I want to sell
-
         
         public string BuyCurrency { get; set; } // must be GRFT
         public decimal BuyAmount { get; set; } // how many Grafts I want to buy
